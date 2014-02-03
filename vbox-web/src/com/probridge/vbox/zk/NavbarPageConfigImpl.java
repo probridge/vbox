a23e1de9c4d97b9d96c5f801e33fd07075e5e91b
George <pennyge@gmail.com>
2014-02-03 09:51:32 +0800

    version 2.0.2 base commit, with non-standard dynamic web project nature.

--- vbox-web/src/com/probridge/vbox/zk/NavbarPageConfigImpl.java
+++ vbox-web/src/com/probridge/vbox/zk/NavbarPageConfigImpl.java
@@ -17,7 +17,7 @@ public class NavbarPageConfigImpl implements NavbarPageConfig {
 		pageMap.put("fn5", new NavbarPage("fn5", "审核中心", "/imgs/fn.png", "approval_mgmt.zul"));
 		pageMap.put("fn6", new NavbarPage("fn6", "母盘管理", "/imgs/fn.png", "golden_master_mgmt.zul"));
 		pageMap.put("fn7", new NavbarPage("fn7", "系统设置", "/imgs/fn.png", "configuration.zul"));
-		pageMap.put("fn8", new NavbarPage("fn8", "管理控制", "/imgs/fn.png", "sysop.zul"));
+//		pageMap.put("fn8", new NavbarPage("fn8", "管理控制", "/imgs/fn.png", "sysop.zul"));
 		pageMap.put("fn9", new NavbarPage("fn9", "控制台", "/imgs/site.png", "console_access.zul"));
 		pageMap.put("fn20", new NavbarPage("fn20", "安全退出", "/imgs/fn.png", "../logout.do?mgmt=1"));
 	}
